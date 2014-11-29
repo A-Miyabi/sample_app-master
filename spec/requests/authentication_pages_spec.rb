dc7f8e95e3a9d816f28bf7c48dee019d2c2d4c19
Kenta <info@e-nishi.net>
2014-11-29 16:09:00 +0900

    Finish sign in

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -17,7 +17,7 @@ describe "Authentication" do
       before { click_button "Sign in" }
 
       it { should have_title('Sign in') }
-      it { should have_selector('div.alert.alert-error', text: 'Invalid') }
+      it { should have_error_message('Invalid') }
 
       describe "after visiting another page" do
         before { click_link "Home" }
@@ -32,6 +32,7 @@ describe "Authentication" do
         fill_in "Password", with: user.password
         click_button "Sign in"
       end
+      #before { valid_signin(user) }
 
       it { should have_title(user.name) }
       it { should have_link('Profile', href: user_path(user)) }
--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -11,3 +11,8 @@ RSpec::Matchers.define :have_error_message do |message|
     expect(page).to have_selector('div.alert.alert-error', text: message)
   end
 end
+Rspec::Matchers.define :have_success_message do |message|
+	match do |page|
+	expect(page).to have_selector('div.alert.alert-success',text: message)
+	end
+end
